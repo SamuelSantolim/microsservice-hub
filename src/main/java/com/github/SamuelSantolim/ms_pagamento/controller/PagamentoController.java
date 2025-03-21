df78dad52f29a4ed383ecdfc898dae2f851469d4
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-21 10:24:41 -0300

    feat: add getAll method em PagamentoController

---
+++ src/main/java/com/github/SamuelSantolim/ms_pagamento/controller/PagamentoController.java
@@ -0,0 +1,28 @@
+package com.github.SamuelSantolim.ms_pagamento.controller;
+
+import com.github.SamuelSantolim.ms_pagamento.dto.PagamentoDTO;
+import com.github.SamuelSantolim.ms_pagamento.service.PagamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/pagamentos")
+public class PagamentoController {
+
+    @Autowired
+    private PagamentoService service;
+
+    @GetMapping
+    public ResponseEntity<List<PagamentoDTO>>getALl(){
+        List<PagamentoDTO> dto = service.getAll();
+        return ResponseEntity.ok(dto);
+
+    }
+
+
+}
